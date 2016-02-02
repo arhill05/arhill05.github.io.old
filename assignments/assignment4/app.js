6ae3b016d425dc178c30f8988fa6df496d1db630
Andrew Hill <mdude2314@gmail.com>
2016-02-01 21:26:19 -0500

    Completed Module 3

--- assignments/assignment4/app.js
+++ assignments/assignment4/app.js
@@ -25,10 +25,11 @@
   });
   
   app.controller('ReviewController', function(){
-   this.review = {};
+	this.review = {};
     this.addReview = function(product){
-      product.reviews.push(this.review);
-      this.review = {};
+		this.review.createdOn = Date.now();
+    product.reviews.push(this.review);
+    this.review = {};
     };
   });
 
