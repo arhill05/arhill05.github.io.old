d00f6e6c698258a281aaf93fa9a7eaa949af93aa
Andrew Hill <mdude2314@gmail.com>
2016-02-01 21:08:33 -0500

    Another committ

--- assignments/assignment4/app.js
+++ assignments/assignment4/app.js
@@ -23,6 +23,14 @@
       return this.tab === tabName;
     };
   });
+  
+  app.controller('ReviewController', function(){
+   this.review = {};
+    this.addReview = function(product){
+      product.reviews.push(this.review);
+      this.review = {};
+    };
+  });
 
   var gems = [
     {
